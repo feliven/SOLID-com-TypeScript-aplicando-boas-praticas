c84ecbe80eb0119d46cfa0939619d4e4adfc4bfb
FVS-MBA <feliven@gmail.com>
2026-05-31 13:44:15 -0300

    desacopla as duas classes

--- src/exemplos/coupling-data.ts
+++ src/exemplos/coupling-data.ts
@@ -1,20 +1,25 @@
-class Usuario {
-  constructor(private nome: string) {}
+interface IUsuario {
+  nome: string;
+  idade?: number;
+}
 
-  getNome(): string {
-    return this.nome;
+class Usuario {
+  constructor(private usuario: IUsuario) {}
+
+  getDados(): IUsuario {
+    return this.usuario;
   }
 }
 
 class GerenciadorUsuario {
-  constructor(private usuario: Usuario) {}
+  constructor(private usuario: IUsuario) {}
 
   mostrarNomeUsuario(): void {
-    console.log(this.usuario.getNome());
+    console.log(this.usuario.nome);
   }
 }
 
 // Utilização dos Módulos A e B
-const usuario = new Usuario("Ana");
-const gerenciadorUsuario = new GerenciadorUsuario(usuario);
+const usuario = new Usuario({ nome: "Ana" });
+const gerenciadorUsuario = new GerenciadorUsuario(usuario.getDados());
 gerenciadorUsuario.mostrarNomeUsuario();
